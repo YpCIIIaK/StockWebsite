f8d67e1d32bf0d9a02500db775a142f39b81d6e6
YpCIIIaK <bigboyvova01@gmail.com>
2024-10-22 20:50:13 +0700

    some changes

--- src/components/Pages/About/AboutPage.jsx
+++ src/components/Pages/About/AboutPage.jsx
@@ -3,12 +3,21 @@ import TextComponent from "../../text/TextComponent";
 
 const AboutPage = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center bg-gray-100 p-6">
+        <div className="min-h-screen flex flex-col items-center bg-gray-100 p-20">
+            <div className='mb-14'>
+                <TextComponent styleType="black" size="xl4">
+                    About the Project
+                </TextComponent>
+                <p className="text-gray-700 text-lg max-w-4xl mx-auto mt-6">
+                    Our project is designed to deliver full stock analysis across all levels for any purpose. It enables users to build and track portfolios, follow specific stocks, and develop strategies based on comprehensive data analysis. With the help of our custom-built AI, users can accelerate their decision-making process and gain deeper insights into stock performance, making it easier to achieve their financial goals.
+                </p>
+            </div>
+
             <div className="text-center mb-10">
                 <TextComponent styleType="black" size="xl4">
                     Meet the Team Behind TraderKek
                 </TextComponent>
-                <p className="text-lg text-gray-600 mt-2">
+                <p className="text-lg text-gray-600 mt-4">
                     Two developers on a mission to revolutionize stock analysis with the power of Artificial Intelligence.
                 </p>
             </div>
