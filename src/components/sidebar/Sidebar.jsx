533d59530c560fe853979563786150005345831d
YpCIIIaK <bigboyvova01@gmail.com>
2024-10-28 18:24:41 +0700

    some changes

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaChartPie } from 'react-icons/fa'; // Импортируем иконку
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div
-            className={`transition-all duration-300 ${isOpen ? 'w-64' : 'w-10'} h-screen bg-gray-800 text-white flex flex-col`}
+            className={`transition-all duration-300 ${isOpen ? 'w-52' : 'w-14'} h-screen bg-gray-800 text-white flex flex-col`}
             onMouseEnter={() => setIsOpen(true)}
             onMouseLeave={() => setIsOpen(false)}
         >
             <NavLink
                 to={'/portfolio/overview'}
                 className={({ isActive }) =>
-                    isActive ? 'bg-gray-700 p-4' : 'p-4 hover:bg-gray-600'
+                    isActive ? 'bg-gray-700 p-4 flex items-center' : 'p-4 flex items-center hover:bg-gray-600'
                 }
             >
-                {isOpen && 'Overview'}
+                <div className="w-12 flex justify-center">
+                    <FaChartPie className="text-xl" />
+                </div>
+                <span
+                    className={`ml-2 overflow-hidden transition-all duration-300 ${isOpen ? 'opacity-100' : 'opacity-0'}`}
+                    style={{ width: isOpen ? 'auto' : '0' }}
+                >
+                    Overview
+                </span>
             </NavLink>
         </div>
     );
